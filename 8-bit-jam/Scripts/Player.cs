68317432b374d80d831c5ee5de4dc16080ac7f88
lilou_cormic <lilou_cormic@hotmail.com>
2025-03-22 15:35:51 -0400

    Available move indicator

--- 8-bit-jam/Scripts/Player.cs
+++ 8-bit-jam/Scripts/Player.cs
@@ -9,6 +9,19 @@ public partial class Player : Node2D
 {
     public MazeLocation Location { get; private set; }
 
+    private Node2D Right;
+    private Node2D Down;
+    private Node2D Left;
+    private Node2D Up;
+
+    public override void _Ready()
+    {
+        Right = GetNode<Node2D>("Right");
+        Down = GetNode<Node2D>("Down");
+        Left = GetNode<Node2D>("Left");
+        Up = GetNode<Node2D>("Up");
+    }
+
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed("ui_right"))
@@ -27,6 +40,11 @@ public partial class Player : Node2D
         {
             TryMove(Direction.Up);
         }
+
+        Right.Visible = GameManager.Maze.CanMove(Location, Direction.Right);
+        Down.Visible = GameManager.Maze.CanMove(Location, Direction.Down);
+        Left.Visible = GameManager.Maze.CanMove(Location, Direction.Left);
+        Up.Visible = GameManager.Maze.CanMove(Location, Direction.Up);
     }
 
     public void SetLocation(MazeLocation location)
